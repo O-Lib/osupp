969dd96c9b25876cdd646fff6c41fee5e5ba6b12
DevilPedrow <60827710+DevilPedrow@users.noreply.github.com>
2026-04-23 21:07:14 +0100

    Fixing features errors

--- osupp/Beatmap/section/timing_points/__init__.py
+++ osupp/Beatmap/section/timing_points/__init__.py
@@ -2,6 +2,7 @@ from .control_points.difficulty import DifficultyPoint
 from .control_points.effect import EffectPoint
 from .control_points.sample import SamplePoint
 from .control_points.timing import TimeSignature, TimeSignatureError, TimingPoint
+
 from .decode import (
     ControlPoint,
     ControlPoints,
--- osupp/Beatmap/section/timing_points/control_points/__init__.py
+++ osupp/Beatmap/section/timing_points/control_points/__init__.py
@@ -3,10 +3,10 @@ from .effect import EffectPoint
 from .sample import SamplePoint
 from .timing import TimeSignature, TimingPoint
 
-__all__ = {
+__all__ = [
     "TimingPoint",
     "TimeSignature",
     "DifficultyPoint",
     "EffectPoint",
     "SamplePoint",
-}
+]
