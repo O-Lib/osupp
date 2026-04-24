5f7c93642c07539fd43dc1e93b25900b8b3c8f75
DevilPedrow <mrpedrito.black@gmail.com>
2026-04-24 19:02:49 +0100

    Fix decode.py

--- osupp/Beatmap/section/hit_objects/decode.py
+++ osupp/Beatmap/section/hit_objects/decode.py
@@ -152,7 +152,7 @@ class HitObjects:
             raise ParseHitObjectsError.hit_object_type(e)
 
         bank_info = SampleBankInfo()
-        inner_kind = None
+        inner_kind: HitObjectCircle | HitObjectSlider | HitObjectSpinner | HitObjectHold | None = None
 
         if hit_object_type.has_flag(HitObjectType.CIRCLE):
             if len(parts) > 5:
